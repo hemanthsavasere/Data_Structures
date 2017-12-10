973e065fc2f3f08bf7d95e2376d32034e17b033e
Hemanth Savasere <hemanth.savasere@gmail.com>
2017-12-10 15:07:58 +0530

    added for ^ symbol also

--- infix_to_postfix.py
+++ infix_to_postfix.py
@@ -1,37 +1,39 @@
 def precedence(symbol):
     if symbol == "+" or symbol == "-":
         return 2
-    elif symbol == "*" or symbol == "/":
+    if symbol == "*" or symbol == "/":
         return 4
+    if symbol == "^":
+        return 8
 
 
 if __name__ == "__main__":
-    infix = raw_input()
-    stack = []
-    postfix = []
-    for i in infix:
-        if i == "(":
-            stack.append(i)
+    for _ in xrange(int(raw_input())):
+            infix = raw_input()
+            stack = []
+            postfix = []
+            for i in infix:
+                if i == "(":
+                    stack.append(i)
 
-        elif i == ")":
-            while stack and stack[len(stack)-1] != "(":
+                elif i == ")":
+                    while stack and stack[len(stack)-1] != "(":
+                        postfix.append(stack.pop())
+
+                    stack.pop()
+
+                elif i == "+" or i == "-" or i == "*" or i == "/" or i == "^":
+
+                    while stack and precedence(i) <= precedence(stack[len(stack) - 1]) :
+                        postfix.append(stack.pop())
+
+
+                    stack.append(i)
+
+                else:
+                    postfix.append(i)
+
+            while stack:
                 postfix.append(stack.pop())
-                print postfix, stack
-            stack.pop()
 
-        elif i == "+" or i == "-" or i == "*" or i == "/":
-
-            while stack and precedence(i) <= precedence(stack[len(stack) - 1]) :
-                postfix.append(stack.pop())
-                print stack
-
-            stack.append(i)
-            print stack, postfix
-
-        else:
-            postfix.append(i)
-
-    while stack:
-        postfix.append(stack.pop())
-
-    print stack, "".join(str(i) for i in postfix)
+            print "".join(str(i) for i in postfix)
